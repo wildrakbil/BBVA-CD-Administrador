b14ea05a3dc5f43b8498183a65ba4adc28d55646
wagarzon <william.garzon.bohorquez.contractro@bbva.com>
2020-02-07 07:48:18 -0500

    inicio ajustes Ux cambio de PC

--- src/app/components/megamenu/megamenu.component.ts
+++ src/app/components/megamenu/megamenu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as classie from 'classie';
 
 @Component({
@@ -8,14 +8,18 @@ import * as classie from 'classie';
 })
 export class MegamenuComponent implements OnInit {
 
+  @Input() navigation_items: any =[];
+  
   constructor() { }
 
   ngOnInit() {
   }
 
   public cleanMegaMenu(){
-  	classie.remove(document.getElementById('megamenu__aside'), 'megamenu--open');
-	  classie.toggle(document.getElementById('megamenu__aside'), 'invisible');
+    classie.remove(document.getElementById('megamenu__aside'), 'megamenu--open');
+    classie.toggle(document.getElementById('megamenu__aside'), 'invisible');
+    classie.toggle(document.getElementById('close'), 'display_none');
+    classie.toggle(document.getElementById('open'), 'display_none');
   }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,29 +8,41 @@ import * as classie from 'classie';
 })
 export class HeaderComponent implements OnInit {
 
+
+  navigation_items:any =[
+  {"navigation__text":"Edición de documentos parametría","routerLink":"/edicion-documentos-parametria" },
+{"navigation__text":"Edición de autorizaciones","routerLink":"/autorizaciones" },
+{"navigation__text":"Asignación perfiles usuario","routerLink":"/asignacion-perfiles" },
+{"navigation__text":"Edición documentos electrónicos","routerLink":"/edicion-documentos-electronicos" },
+{"navigation__text":"Editar formato información pre-oferta","routerLink":"/editar-pre-oferta" },
+{"navigation__text":"Editar formato información oferta","routerLink":"/editar-oferta" },
+{"navigation__text":"Edición de preguntas de control","routerLink":"/preguntas-de-control" },
+{"navigation__text":"Dar de baja candidatos > a 60 dias","routerLink":"/candidatos" },
+{"navigation__text":"Cerrar sesión","routerLink":"/home" }
+  ];
+
+  userName:string='Pepito Rodríguez';
+
   constructor() { }
 
   ngOnInit() {
   }
 
-   public searchPrincipal(siguiente: string) {
-  	classie.toggle(document.getElementById(siguiente), 'search--open');
-  	document.getElementById(siguiente).style.visibility = "visible";
-  	classie.remove(document.getElementById('megamenu__aside'), 'megamenu--open');
-	  classie.toggle(document.getElementById('megamenu__aside'), 'invisible');
+  public searchPrincipal(siguiente: string) {
+    classie.toggle(document.getElementById(siguiente), 'search--open');
+    document.getElementById(siguiente).style.visibility = "visible";
+    classie.remove(document.getElementById('megamenu__aside'), 'megamenu--open');
+    classie.toggle(document.getElementById('megamenu__aside'), 'invisible');
     document.getElementById('serach_principal').style.display = "block";
     document.getElementById('serach_secundario').style.display = "none";
   }
-      
-   public menuPrincipal(siguiente: string) {
-	  classie.toggle(document.getElementById(siguiente), 'megamenu--open');
-	  classie.remove(document.getElementById(siguiente), 'invisible');
+
+  public menuPrincipal(siguiente: string) {
+    classie.toggle(document.getElementById(siguiente), 'megamenu--open');
+    classie.remove(document.getElementById(siguiente), 'invisible');
     classie.remove(document.getElementById('search__aside'), 'search--open');
-	  document.getElementById('search__aside').style.visibility = "visible";
-   }
-
-   public hola(){
-     
-   }
-
+    document.getElementById('search__aside').style.visibility = "visible";
+    classie.toggle(document.getElementById('close'), 'display_none');
+    classie.toggle(document.getElementById('open'), 'display_none');
+  }
 }
